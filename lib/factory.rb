f8f92b2b567c9e3a68c97c7c2e6e752252241c5e
Denis Zemlianoy <dzemlianoi@gmail.com>
2018-10-31 16:25:45 +0200

    Ruby factory added

---
+++ lib/factory.rb
@@ -0,0 +1,16 @@
+# * Here you must define your `Factory` class.
+# * Each instance of Factory could be stored into variable. The name of this variable is the name of created Class
+# * Arguments of creatable Factory instance are fields/attributes of created class
+# * The ability to add some methods to this class must be provided while creating a Factory
+# * We must have an ability to get/set the value of attribute like [0], ['attribute_name'], [:attribute_name]
+#
+# * Instance of creatable Factory class should correctly respond to main methods of Struct
+# - each
+# - each_pair
+# - dig
+# - size/length
+# - members
+# - select
+# - to_a
+# - values_at
+# - ==, eql?
